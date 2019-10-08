3daf39f28acd351a310d73dfd662f6e371728ad6
Mahroz <mahroz.jawad16@iesdoctorbalmis.com>
2019-10-08 11:34:01 +0200

    Hecho

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -27,26 +27,7 @@ namespace Calculator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (((Button)sender).Name == "uno")
-                Texto.Text = Texto.Text + "1";
-            if (((Button)sender).Name == "dos")
-                Texto.Text = Texto.Text + "2";
-            if (((Button)sender).Name == "tres")
-                Texto.Text = Texto.Text + "3";
-            if (((Button)sender).Name == "cuatro")
-                Texto.Text = Texto.Text + "4";
-            if (((Button)sender).Name == "cinco")
-                Texto.Text = Texto.Text + "5";
-            if (((Button)sender).Name == "seis")
-                Texto.Text = Texto.Text + "6";
-            if (((Button)sender).Name == "siete")
-                Texto.Text = Texto.Text + "7";
-            if (((Button)sender).Name == "ocho")
-                Texto.Text = Texto.Text + "8";
-            if (((Button)sender).Name == "nueve")
-                Texto.Text = Texto.Text + "9";
-            if (((Button)sender).Name == "cero")
-                Texto.Text = Texto.Text + "0";
+            Texto.Text = Texto.Text + ((Button)sender).Tag.ToString();
         }
     }
 }
